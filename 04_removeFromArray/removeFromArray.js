dc02967fb059606741850dc94fc077cc980b62cb
Ken Henkel <kenhenkel95@gmail.com>
2024-11-05 12:06:36 -0500

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...itemsToBeRemoved) {
+    
+    for(let i = inputArray.length - 1; i >= 0; i--){
+        for(item of itemsToBeRemoved){
+            if(inputArray[i] === item){
+                inputArray.splice(i, 1);
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
