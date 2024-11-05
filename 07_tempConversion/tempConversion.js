81d6a90b45137246b5d8f3b0f01296313c70c40c
Ken Henkel <kenhenkel95@gmail.com>
2024-11-05 13:15:26 -0500

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempToConvert) {
+  tempToConvert = (tempToConvert - 32) / 1.8;
+  return Math.round(tempToConvert * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToConvert) {
+  tempToConvert = tempToConvert * 1.8 + 32;
+  return Math.round(tempToConvert * 10) / 10;
 };
 
 // Do not edit below this line
