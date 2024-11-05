a3e44ebe09d249792a4bc8fb13168b7293832b26
Ken Henkel <kenhenkel95@gmail.com>
2024-11-05 12:35:29 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let addedNum = 0;
+    if(startNum > endNum)[startNum, endNum] = [endNum, startNum];
+    if(startNum < 0 || endNum < 0) return 'ERROR';
+    if(!Number.isInteger(startNum) || !Number.isInteger(endNum)) return 'ERROR';
+    for(let i = startNum; i <= endNum; i++){
+        addedNum += i;
+    }
+    return addedNum;
 };
 
 // Do not edit below this line
