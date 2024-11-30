842083bd2bdf753774ac49fb90afe367198333d4
Ken Henkel <kenhenkel95@gmail.com>
2024-11-30 14:28:17 -0500

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((result, current) => result * current, 1); 
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+	return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i = num; i >= 1; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
