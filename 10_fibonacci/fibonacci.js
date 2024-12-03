8a9028a0f14a364ccc4a6b338d208341638d145b
Ken Henkel <kenhenkel95@gmail.com>
2024-12-03 12:34:41 -0500

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    //I think this needs to be refactored using reduce with an array of [1] and initial value 1. Maybe?
+    let arr = [0, 1];
+    if(num < 0){
+        return "OOPS";
+    }
+    for(let i = 1; i < num; i++){
+        let newNum = arr[i] + arr[i-1];
+        arr.push(newNum);
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
