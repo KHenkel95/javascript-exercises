0017fa4ea3ef9457ba77db44febad8b506d01ad5
Ken Henkel <kenhenkel95@gmail.com>
2024-12-02 19:33:38 -0500

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~]/g, "").replace(/\s/g, "");
+    const reverseStr = cleanedStr.split('').reverse().join('');
+    return cleanedStr === reverseStr ? true : false;
 };
 
 // Do not edit below this line
